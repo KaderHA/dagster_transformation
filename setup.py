cc388da99e9fa6f16fc04732879d005a53b4d39e
KaderHA <kader.hussein.abdi@norges-bank.no>
2024-08-06 15:01:19 +0200

    chore: add version to dagster-databricks package

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name="transformation",
     packages=find_packages(exclude=["transformation_tests"]),
-    install_requires=["dagster==1.7.15", "dagster-cloud==1.7.15", "dagster-databricks"],
+    install_requires=[
+        "dagster==1.7.15",
+        "dagster-cloud==1.7.15",
+        "dagster-databricks==0.23.15",
+    ],
     extras_require={"dev": ["dagster-webserver==1.7.15", "pytest"]},
 )
