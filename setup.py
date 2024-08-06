e6eedefbae59f0e3b5f4faf7286610b6d90e709b
annabdoan <anna.doan@norges-bank.no>
2024-08-06 12:47:48 +0200

    fix: match versions

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="transformation",
     packages=find_packages(exclude=["transformation_tests"]),
-    install_requires=["dagster==1.7.15", "dagster-cloud==1.7.15"],
-    extras_require={"dev": ["dagster-webserver", "pytest"]},
+    install_requires=["dagster==1.7.15", "dagster-cloud==1.7.15", "dagster-databricks"],
+    extras_require={"dev": ["dagster-webserver==1.7.15", "pytest"]},
 )
